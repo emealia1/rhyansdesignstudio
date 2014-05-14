ba6fce0cf72c9026d4c0f18b01fe876b5b106166
ksuwdc <kyle.kinnaman@ksuwd.com>
2014-05-14 19:48:36 -0400

    Vance new site

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,39 @@
-var firstView=document.getElementById('firstView');firstView.parentNode.removeChild(firstView);angular.module('app',['ui.router','ngAnimate']).config(['$urlRouterProvider','$stateProvider','$locationProvider',function(a,b,c){a.otherwise('/');b.state('home',{url:'/',views:{"main":{templateUrl:'templates/home.html'}}}).state('menu',{views:{"menu":{templateUrl:'templates/menu.html'}}}).state('services',{url:'/services',abstract:true,views:{"main":{templateUrl:'templates/services.html'}}}).state('services.index',{url:'/',views:{"services":{templateUrl:'templates/services.index.html'}}}).state('services.custom',{url:'/custom',views:{"services":{templateUrl:'templates/services.custom.html'}}}).state('services.repairs',{url:'/repairs',views:{"services":{templateUrl:'templates/services.repairs.html'}}}).state('services.redesign',{url:'/redesign',views:{"services":{templateUrl:'templates/services.redesign.html'}}}).state('about',{url:'/about',views:{"main":{templateUrl:'templates/about.html'}}})}]);
+angular
+  .module('app', [
+    'ui.router',
+    'ngAnimate'
+  ])
+  .config(['$urlRouterProvider', '$stateProvider', '$locationProvider', function($urlRouterProvider, $stateProvider, $locationProvider) {
+    // $locationProvider.html5Mode(true);
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+      .state('home', {
+        url: '/',
+        views: {
+          "main": {
+            templateUrl: 'templates/home.html'
+          }
+        }
+      })
+      .state('products', {
+        views: {
+          "main": {
+            templateUrl: 'templates/products.html'
+          }
+        }
+      })
+      .state('lessons', {
+        views: {
+          "main": {
+            templateUrl: 'templates/lessons.html'
+          }
+        }
+      })
+      .state('contact', {
+        views: {
+          "main": {
+            templateUrl: 'templates/contact.html'
+          }
+        }
+      });
+    }]);
